af1a513ec7737a41cd3e90b2037ce894611868f6
Yuriy Bondarenko <y_bondarenko@wargaming.net>
2022-05-27 22:11:35 +0300

    generateParenthesis

--- generate-parentheses.js
+++ generate-parentheses.js
@@ -6,14 +6,14 @@
 var generateParenthesis = function (n) {
   let res = [];
 
-  const generate = function (n, curr = "", opened = 0, closed = 0, res = []) {
+  const generate = function (n, curr = "", opened = 0, closed = 0) {
     if (curr.length === 2 * n) {
       return res.push(curr);
     }
     if (opened < n) {
       generate(n, curr + "(", opened + 1, closed);
     }
-    if (closed < n) {
+    if (closed < opened) {
       generate(n, curr + ")", opened, closed + 1);
     }
   };
