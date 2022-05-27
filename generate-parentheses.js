7090507ab7868c49437304d7674e3b91465c353a
Yuriy Bondarenko <y_bondarenko@wargaming.net>
2022-05-27 07:48:40 +0300

    generateParenthesis

---
+++ generate-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/generate-parentheses/
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  let res = [];
+
+  const generate = function (n, curr = "", opened = 0, closed = 0, res = []) {
+    if (curr.length === 2 * n) {
+      return res.push(curr);
+    }
+    if (opened < n) {
+      generate(n, curr + "(", opened + 1, closed);
+    }
+    if (closed < n) {
+      generate(n, curr + ")", opened, closed + 1);
+    }
+  };
+
+  generate(n);
+  return res;
+};
+
+console.log(generateParenthesis(3));
